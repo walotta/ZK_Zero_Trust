2c6e364651f098a8f2540bcbb3b75eea5e739f74
walotta <walotta2001@gmail.com>
2026-02-02 18:14:11 -0600

    update

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,17 +3,13 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Default)]
 #[serde(default)]
 pub struct Inputs {
-    pub access_subject_bogus: String,
-    pub resource_resource_id: String,
-    pub action_action_id: String,
+    pub access_subject_subject_id: String,
 }
 
 impl Inputs {
-    pub fn new(access_subject_bogus: String, resource_resource_id: String, action_action_id: String) -> Self {
+    pub fn new(access_subject_subject_id: String) -> Self {
         Self {
-            access_subject_bogus,
-            resource_resource_id,
-            action_action_id,
+            access_subject_subject_id,
         }
     }
 }
